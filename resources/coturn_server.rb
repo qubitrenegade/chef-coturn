b82807b76207812de17e717c172da793defc656f
qubitrenagade <qubitrenegade@gmail.com>
2019-08-23 09:41:02 -0600

    take resource out

--- resources/coturn_server.rb
+++ resources/coturn_server.rb
@@ -4,17 +4,16 @@ resource_name :coturn_server
 property :server_name, String, name_property: true
 
 action :create do
-  certbot_exec new_resource.server_name
   
   package 'coturn' do
     action :install
   end
 
-  # file '/etc/default/coturn' do
-  #   content "TURNSERVER_ENABLED=1\n"
-  #   mode '0644'
-  #   action :create
-  # end
+  file '/etc/default/coturn' do
+    content "TURNSERVER_ENABLED=1\n"
+    mode '0644'
+    action :create
+  end
 
   template '/etc/turnserver.conf' do
     source 'turnserver.conf.erb'
@@ -23,7 +22,6 @@ action :create do
     )
     cookbook 'coturn'
     action :create
-    notifies :exec, "certbot_exec[#{new_resource.server_name}]", :before
     notifies :restart, 'service[coturn]', :delayed
   end
 
